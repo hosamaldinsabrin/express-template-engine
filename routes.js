7123b9055cf2a304fe993edb9b082ab7948c6b24
fold4wrap5 <nielspoensgen@protonmail.com>
2020-07-01 12:20:09 +0200

    initial commit

---
+++ routes.js
@@ -0,0 +1,48 @@
+// routes/things.js routing file
+"use strict";
+const express = require("express");
+const router = express.Router();
+
+router.use(function(req, res, next) {
+  console.log(req.url, "@", Date.now());
+  next();
+});
+
+router
+  .route("/")
+  .get((req, res) => {
+    // Hier sollen alle datenbank daten zurück gegeben werden.
+  })
+
+router
+  .route("/music")
+  .get((req, res) => {
+    // Hier sollen alle Musik Alben zurück gegegeben werden
+  })
+  .post((req, res) => {
+    // Hier soll ein neues Album mit einer neuen, einzigartigen uuid eingefügt werden.
+  });
+
+  router
+  .route("/films")
+  .get((req, res) => {
+    // Hier sollen alle Filme zurück gegegeben werden
+  })
+  .post((req, res) => {
+    // Hier soll ein neuer Film mit einer neuen, einzigartigen uuid eingefügt werden.
+  });
+
+router
+  .route("/music/:id")
+  .get((req, res) => {
+    // Hier soll ein einziges Album anhand einer ID zurückgegeben werden.
+  })
+
+  router
+  .route("/film/:id")
+  .get((req, res) => {
+    // Hier soll ein einziger FIlm anhand einer ID zurückgegeben werden.
+  })
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const api = require("./routes");
+const { response } = require("express");
+
+app.use(express.json());
+app.use("/api", api);
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+  res.end();
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
